5560388e62019b6f23f1c8fd6686fa7cd973045d
Piyush <chunarapiyush10@gmail.com>
2025-05-03 00:26:14 +0530

    making tabbar in New and Hot page

--- lib/screens/new_and_hot.dart
+++ lib/screens/new_and_hot.dart
@@ -5,6 +5,56 @@ class NewAndHot extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Hot & New", style: TextStyle(color : Colors.white,),),
+          actions: [
+            IconButton(
+              onPressed: (){},
+              icon: Icon(Icons.cast, color: Colors.white),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 20.0, left: 25),
+              child: Container(
+                height: 25,
+                width: 25,
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(5),
+                ),
+              ),
+            ),
+          ],
+          bottom: TabBar(
+            labelStyle: TextStyle(fontSize: 18, color : Colors.black, fontWeight: FontWeight.bold),
+            indicatorAnimation: TabIndicatorAnimation.linear,
+            indicator: BoxDecoration(
+              color : Colors.white,
+              borderRadius: BorderRadius.circular(15)
+
+            ),
+            tabs: [
+              Tab(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal:  10.0),
+                  child: Text("🍿 Comming Soon"),
+                ),
+              ),
+              Tab(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal:  10.0),
+                  child: Text("🔥 Everyone's Watching"),
+                ),
+              ),
+          ]),
+        ),
+        body: TabBarView(children: [
+          Center(child: Text("1"),),
+          Center(child: Text("2"),),
+        ]),
+      ),
+    );
   }
 }
